4c6ff37272dfff86067f1bb008143f4d8a1f35dd
Chris Evans <chrise@vmware.com>
2020-06-03 13:50:22 +0930

    No wget

--- Folding/run-1.sh
+++ Folding/run-1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get update
-wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/fahclient_7.6.13_amd64.deb
-wget https://download.foldingathome.org/releases/public/release/fahcontrol/debian-stable-64bit/v7.6/fahcontrol_7.6.13-1_all.deb
-wget https://download.foldingathome.org/releases/public/release/fahviewer/debian-stable-64bit/v7.6/fahviewer_7.6.13_amd64.deb
+#wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.6/fahclient_7.6.13_amd64.deb
+#wget https://download.foldingathome.org/releases/public/release/fahcontrol/debian-stable-64bit/v7.6/fahcontrol_7.6.13-1_all.deb
+#wget https://download.foldingathome.org/releases/public/release/fahviewer/debian-stable-64bit/v7.6/fahviewer_7.6.13_amd64.deb
 sudo dpkg -i --force-depends fahclient_7.6.13_amd64.deb
 sudo dpkg -i --force-depends fahcontrol_7.6.13-1_all.deb
 sudo dpkg -i --force-depends fahviewer_7.6.13_amd64.deb
