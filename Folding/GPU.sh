aa4a612e16bfac3b51c5f65aa02e5ac7de26cff6
Chris Evans <chrise@vmware.com>
2020-06-11 15:21:02 +0930

    Create GPU.sh
    GPU automation

---
+++ Folding/GPU.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt install ubuntu-drivers-common --yes
+ubuntu-drivers devices
+sudo ubuntu-drivers autoinstall
+sudo apt install ocl-icd-libopencl1 --yes
+sudo apt install ocl-icd-opencl-dev --yes
+reboot --yes
