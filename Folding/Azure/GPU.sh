b1337f3cddcd6e8ad63f118a7c76e6bf60eede3a
Chris <50994413+chrise01@users.noreply.github.com>
2021-09-10 08:03:21 +0930

    Driver changes

--- Folding/Azure/GPU.sh
+++ Folding/Azure/GPU.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 apt install ubuntu-drivers-common --yes
 ubuntu-drivers devices
-sudo ubuntu-drivers autoinstall
+sudo apt update
+sudo apt upgrade
+sudo apt install nvidia-driver-460
+#sudo ubuntu-drivers autoinstall
 sudo apt install ocl-icd-libopencl1 --yes
 sudo apt install ocl-icd-opencl-dev --yes
 reboot
